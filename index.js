c87ddebad91cfbea821298c4b04b178e5832acc6
ghpages-test <76713010+ghpages-test@users.noreply.github.com>
2020-12-29 10:50:30 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ function unserialize(qstr) {
   return p;
 }
 
+function callApi(url, token, callback) {
+  $.ajax({
+    url: url + (url.indexOf("?") >= 0 ? "&" : "?") + "access_token=" + token,
+    data: null,
+    success: callback,
+    dataType: "jsonp",
+  });
+}
+
 if (location.hash.substr() != "") {
   var params = unserialize(location.hash.substr(1));
   console.log(params);
+  const btn = document.getElementById("query-btn");
+  const apiUrl = "http://twinoid.com/graph/me";
+  btn.addEventListener(
+    "click",
+    callApi(apiUrl, params.access_token, (obj) => {
+      console.log(obj);
+    })
+  );
 }
